87fa95fbf6a8e8eb40bcb98b70b7defdbc290c46
KSOL <ksol95@ya.ru>
2024-03-12 16:51:07 +0400

    Исправил комментарии

--- scripts/dist/index.dev.js
+++ scripts/dist/index.dev.js
@@ -1,19 +1,22 @@
-const cardTemplate = document.querySelector("#card-template").content;
-const placesList = document.querySelector(".places__list");
+"use strict";
 
-const deleteCard = (evt) => {
+var cardTemplate = document.querySelector("#card-template").content;
+var placesList = document.querySelector(".places__list");
+
+var deleteCard = function deleteCard(evt) {
   evt.target.parentElement.remove();
 };
 
 function createCard(template, cardLink, cardName, del) {
-  const card = template.querySelector(".card").cloneNode(true);
+  var card = template.querySelector(".card").cloneNode(true);
   card.querySelector(".card__image").src = cardLink;
   card.querySelector(".card__image").setAttribute("alt", cardName);
   card.querySelector(".card__title").textContent = cardName;
   card.querySelector(".card__delete-button").addEventListener("click", del);
   return card;
-}
-//вывод всех карточек из массива
-initialCards.forEach((el) => {
+} //вывод всех карточек из массива
+
+
+initialCards.forEach(function (el) {
   placesList.append(createCard(cardTemplate, el.link, el.name, deleteCard));
 });
